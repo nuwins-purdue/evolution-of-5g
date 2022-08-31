ab8ad1897f6d780cbe5fd9be202c6de1d5456788
imranbuet63 <imranbuet63@gmail.com>
2022-08-31 14:12:23 -0400

    temp

---
+++ copy_scripts.sh
@@ -0,0 +1,12 @@
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Denmark/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Deutschland/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Germany/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Greece/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/India/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Luxembourg/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Netherlands/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/UK/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/United\ Kingdom/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/United\ States\ of\ America/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/United\ States/
+cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/USA/
