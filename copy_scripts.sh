d13d265d6b1d58199dd8071a654d987c35e4a539
imranbuet63 <imranbuet63@gmail.com>
2022-09-08 19:22:21 -0400

    updated sept 8

--- copy_scripts.sh
+++ copy_scripts.sh
@@ -1,14 +1,14 @@
 cp -r ../Output/* ./images/performance_plots/
-cp images/performance_plots/United\ States/* images/performance_plots/United\ States\ of\ America/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Denmark/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Deutschland/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Germany/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Greece/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/India/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Luxembourg/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Netherlands/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/UK/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/United\ Kingdom/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/United\ States\ of\ America/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/United\ States/
-cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/USA/
+#cp images/performance_plots/United\ States/* images/performance_plots/United\ States\ of\ America/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Denmark/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Deutschland/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Germany/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Greece/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/India/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Luxembourg/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/Netherlands/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/UK/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/United\ Kingdom/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/United\ States\ of\ America/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/United\ States/
+#cp images/performance_plots/worldwide/worlwide_stats.jpg images/performance_plots/USA/
